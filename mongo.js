4d944f67588b73feaa73704caabd43026101d393
Steve Lasker <StevenLasker@Hotmail.com>
2017-03-22 10:41:48 -0700

    Update connection string

--- mongo.js
+++ mongo.js
@@ -1,2 +1,2 @@
-process.env.MONGO_DB_CONNECTION_STRING = "mongodb://contosobikerental:drmPbcmDf1wWQomIiCDqtp2AnKopQS5gk5pKHcDniLtpxwv8p1FdTqXdNEs69jpedrk5g08uJ24z1rcmOHBKOQ==@contosobikerental.documents.azure.com:10250/?ssl=true";
+process.env.MONGO_DB_CONNECTION_STRING = "mongodb://stevelas:yG7eeaAjNZEsiVs7RyuZoMnjh1ghTmNX4K4kebtA1EFYtAJpzzWK0KBMq7pnJXAcagSKiXol1jNV2qv3TBohXQ==@stevelas.documents.azure.com:10250/?ssl=true";
 process.env.MONGO_DB_COLLECTION = "bikes";
